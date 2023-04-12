2666cff351dd11a1f44fed7dc7a0e0746b5b8f69
NazNaheed <75083482+NazNaheed@users.noreply.github.com>
2023-04-12 14:14:31 +0200

    Indentation and comments

--- InheritanceExample/BaseDerviedProgram.cs
+++ InheritanceExample/BaseDerviedProgram.cs
@@ -1,42 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-            BaseClass bc = new BaseClass();
-            DerivedClass dc = new DerivedClass();
-            BaseClass bcdc = new DerivedClass();
-         
-
-                bc.Method1();
-                bc.Method2();
-
+﻿
+BaseClass bc = new BaseClass();
+DerivedClass dc = new DerivedClass();
+BaseClass bcdc = new DerivedClass();
+// calling the function of base class 
+bc.Method1();
+bc.Method2();
+// calling the functions of derived class
 dc.Method1();
 dc.Method2();
-
+// calling the functions of dervied class
 bcdc.Method1();
 bcdc.Method2();
 
 
-
 class BaseClass
+{
+    public virtual void Method1()
     {
-        public virtual void Method1()
-        {
-            Console.WriteLine("Base - Method1");
-        }
-
-        public virtual void Method2()
-        {
-            Console.WriteLine("Base - Method2");
-        }
+        Console.WriteLine("Base - Method1");
     }
 
-    class DerivedClass : BaseClass
+    public virtual void Method2()
     {
-        public override void Method1()
-        {
-            Console.WriteLine("Derived - Method1");
-        }
-
-        public new void Method2()
-        {
-            Console.WriteLine("Derived - Method2");
-        }
+        Console.WriteLine("Base - Method2");
     }
+}
+
+class DerivedClass : BaseClass
+{
+    public override void Method1()
+    {
+        Console.WriteLine("Derived - Method1");
+    }
+
+    public override void Method2()
+    {
+        Console.WriteLine("Derived - Method2");
+    }
+}
